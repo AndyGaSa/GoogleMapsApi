c91c5bd300d6884265f2102c6ebc68396edcb57b
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-11-16 22:20:03 +0100

    Create react app and install eslinter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        aqui va el mapa
+      </body>
+    </div>
+  );
+}
+
+export default App;
