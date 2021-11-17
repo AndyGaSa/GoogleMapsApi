0c40f79827ef93671a31e1a7b4201e6624371453
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-11-18 00:10:21 +0100

    Add titles, render map component and style the App with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
 import './App.css';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
-      <body>
-        aqui va el mapa
-      </body>
+      <h1 data-testid="Title">Google Maps API</h1>
+      <h3>by Andy Garcia</h3>
+      <Map />
     </div>
   );
 }
